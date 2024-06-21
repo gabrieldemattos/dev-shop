ea905c53aa1aa4ccb15f662d9e81468220980e6f
Gabriel de Mattos <mattos_95@hotmail.com>
2024-06-21 17:35:47 -0300

    feat: add category page

---
+++ app/_constants/category-icon.tsx
@@ -0,0 +1,8 @@
+import { Computer, Gamepad2, Laptop, Smartphone } from "lucide-react";
+
+export const CATEGORY_ICON = {
+  "Computadores e Laptops": <Laptop size={20} />,
+  "Games e Consoles": <Gamepad2 size={20} />,
+  "Periféricos para Computadores": <Computer size={20} />,
+  "Smartphones e Tablets": <Smartphone size={20} />,
+};
--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import ProductItem from "@/app/_components/product-item";
+import { Badge } from "@/app/_components/ui/badge";
+import { CATEGORY_ICON } from "@/app/_constants/category-icon";
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
 
-const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+interface CategoryPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const CategoryPage = async ({ params }: CategoryPageProps) => {
+  const category = await db.category.findUnique({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      products: true,
+    },
+  });
+
+  if (!category) return notFound();
+
+  console.log(category.name);
+
+  return (
+    <div className="mx-auto flex flex-col justify-center gap-5 px-5">
+      <Badge variant="outline" className="space-x-2 border-none p-2 text-lg">
+        {CATEGORY_ICON[category.name as keyof typeof CATEGORY_ICON]}
+        <span className="font-semibold uppercase">{category.name}</span>
+      </Badge>
+
+      <div className="mx-auto grid grid-cols-2 gap-8">
+        {category.products.map((product) => (
+          <ProductItem key={product.id} product={{ ...product, category }} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPage;
