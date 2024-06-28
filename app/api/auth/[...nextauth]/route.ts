0c8f79dc39142aa23143363dae99bb689474b1a6
Gabriel de Mattos <mattos_95@hotmail.com>
2024-06-28 16:56:00 -0300

    feat: configure next auth to accept user ID

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,6 @@
+import { authOptions } from "@/app/_lib/auth";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { db } from "@/app/_lib/prisma";
-import { Adapter } from "next-auth/adapters";
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
