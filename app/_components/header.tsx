6e5832ceb8b540e0a064039cac7975226807e97f
Gabriel de Mattos <mattos_95@hotmail.com>
2024-07-03 15:32:52 -0300

    fix: correct style class that was wrong

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -252,7 +252,7 @@ const Header = () => {
                 <Button
                   size="icon"
                   variant="outline"
-                  className="border-none bg-transparent text-white hover:bg-transparent"
+                  className="relative border-none bg-transparent text-white hover:bg-transparent"
                 >
                   {totalProducts > 0 && (
                     <span className="absolute bottom-0 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-foreground p-1 text-xs font-bold text-background">
