fccfd7a4496359387cb3772cc2b4796bf0789cc1
Gabriel de Mattos <mattos_95@hotmail.com>
2024-07-22 17:29:14 -0300

    feat: create component to pass favorite products list as props and display search results

---
+++ app/search/_components/products.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { Product, UserFavoriteProduct } from "@prisma/client";
+import { notFound, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { searchProducts } from "../_actions/search";
+import ProductItem from "@/app/_components/product-item";
+import { Frown } from "lucide-react";
+import Header from "@/app/_components/header";
+import Loader from "@/app/_components/loader";
+
+interface ProductProps {
+  userFavorites: UserFavoriteProduct[];
+}
+
+const Products = ({ userFavorites }: ProductProps) => {
+  const searchParams = useSearchParams();
+  const searchFor = searchParams.get("q");
+
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      if (!searchFor) return;
+
+      setIsLoading(true);
+      try {
+        const foundProducts = await searchProducts(searchFor);
+
+        setProducts(foundProducts);
+      } catch (error) {
+        console.log(error);
+        setProducts([]);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, [searchFor]);
+
+  if (!searchFor) return notFound();
+
+  return (
+    <>
+      <Header />
+
+      <div className="mt-5 flex flex-col space-y-6 px-5">
+        <h2 className="truncate text-xl text-foreground">
+          Resultados para:{" "}
+          <span className="font-bold text-foreground">{searchFor}</span>
+        </h2>
+
+        <div className="grid grid-cols-2 gap-6">
+          {!isLoading &&
+            products.length > 0 &&
+            products.map((product) => (
+              <ProductItem
+                key={product.id}
+                product={JSON.parse(JSON.stringify(product))}
+                className="min-w-full"
+                userFavorites={userFavorites}
+              />
+            ))}
+        </div>
+      </div>
+
+      {!isLoading && products.length === 0 && (
+        <div className="mt-20 flex flex-col items-center gap-5 px-5">
+          <p className="text-base font-bold">
+            Não encontramos nenhum resultado para sua pesquisa!
+          </p>
+
+          <Frown size={150} className="text-gray-400 text-opacity-30" />
+        </div>
+      )}
+
+      {isLoading && <Loader />}
+    </>
+  );
+};
+
+export default Products;
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,76 +1,20 @@
-"use client";
+import Products from "./_components/products";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
+import { db } from "../_lib/prisma";
 
-import { notFound, useSearchParams } from "next/navigation";
-import Header from "../_components/header";
-import { useEffect, useState } from "react";
-import { Product } from "@prisma/client";
-import { searchProducts } from "./_actions/search";
-import ProductItem from "../_components/product-item";
-import { Frown } from "lucide-react";
-import Loader from "../_components/loader";
+const SearchPage = async () => {
+  const session = await getServerSession(authOptions);
 
-const SearchPage = () => {
-  const searchParams = useSearchParams();
-  const searchFor = searchParams.get("q");
-
-  const [products, setProducts] = useState<Product[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      if (!searchFor) return;
-
-      setIsLoading(true);
-      try {
-        const foundProducts = await searchProducts(searchFor);
-
-        setProducts(foundProducts);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-        setProducts([]);
-      }
-    };
-
-    fetchProducts();
-  }, [searchFor]);
-
-  if (!searchFor) return notFound();
+  const userFavorites = await db.userFavoriteProduct.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
 
   return (
     <>
-      <Header />
-
-      <div className="mt-5 flex flex-col space-y-6 px-5">
-        <h2 className="truncate text-xl text-foreground">
-          Resultados para:{" "}
-          <span className="font-bold text-foreground">{searchFor}</span>
-        </h2>
-
-        <div className="grid grid-cols-2 gap-6">
-          {!isLoading &&
-            products.length > 0 &&
-            products.map((product) => (
-              <ProductItem
-                key={product.id}
-                product={JSON.parse(JSON.stringify(product))}
-                className="min-w-full"
-              />
-            ))}
-        </div>
-      </div>
-
-      {!isLoading && products.length === 0 && (
-        <div className="mt-20 flex flex-col items-center gap-5 px-5">
-          <p className="text-base font-bold">
-            Não encontramos nenhum resultado para sua pesquisa!
-          </p>
-
-          <Frown size={150} className="text-gray-400 text-opacity-30" />
-        </div>
-      )}
-
-      {isLoading && <Loader />}
+      <Products userFavorites={userFavorites} />
     </>
   );
 };
