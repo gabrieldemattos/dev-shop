5376cf70438a4f8e5592642edc273a42ac12d9c0
Gabriel de Mattos <mattos_95@hotmail.com>
2024-07-22 17:27:05 -0300

    feat: create server action to toggle favorite product button

---
+++ app/_actions/toggle-favorite-product.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { db } from "../_lib/prisma";
+
+export const toggleFavoriteProduct = async (
+  productId: string,
+  userId: string,
+) => {
+  const isFavorite = await db.userFavoriteProduct.findFirst({
+    where: {
+      userId,
+      productId,
+    },
+  });
+
+  if (isFavorite) {
+    await db.userFavoriteProduct.delete({
+      where: {
+        userId_productId: {
+          userId,
+          productId,
+        },
+      },
+    });
+
+    revalidatePath("/");
+    return;
+  }
+
+  await db.userFavoriteProduct.create({
+    data: {
+      userId,
+      productId,
+    },
+  });
+
+  revalidatePath("/");
+};
