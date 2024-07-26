bc109ad1c2cfa28b07f140cef30a33a61738dc4a
Gabriel de Mattos <mattos_95@hotmail.com>
2024-07-26 18:51:22 -0300

    feat: create server actions and hook for liking/unliking reviews

---
+++ app/_actions/get-user-liked-reviews.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { db } from "../_lib/prisma";
+
+export const getUserLikedReviews = async (userId: string) => {
+  if (!userId) return [];
+
+  const likedReviews = await db.reviewLike.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      reviewId: true,
+    },
+  });
+
+  return likedReviews.map((like) => like.reviewId);
+};
---
+++ app/_hooks/useLikeDeslikeReview.ts
@@ -0,0 +1,50 @@
+import { useSession } from "next-auth/react";
+import { unlikeReview } from "../_actions/review-unlike";
+import { likeReview } from "../_actions/review-like";
+import { toast } from "sonner";
+import { useState } from "react";
+
+export const useLikeDeslikeReview = (
+  reviewId: string,
+  categorySlug: string,
+  productSlug: string,
+  isLike: string | undefined,
+) => {
+  const { data } = useSession();
+
+  const [isLikeDislikeLoading, setIsLikeDislikeLoading] = useState(false);
+
+  const handleLikeUnlikeReviewClick = async () => {
+    if (!data) {
+      toast.error("Você precisa fazer login para curtir uma avaliação.", {
+        position: "bottom-center",
+      });
+      return;
+    }
+
+    setIsLikeDislikeLoading(true);
+    try {
+      if (isLike) {
+        await unlikeReview(
+          data.user.id as string,
+          reviewId,
+          categorySlug,
+          productSlug,
+        );
+      } else {
+        await likeReview(
+          data.user.id as string,
+          reviewId,
+          categorySlug,
+          productSlug,
+        );
+      }
+    } catch (error) {
+      toast.error("Erro ao curtir/descurtir a avaliação");
+    } finally {
+      setIsLikeDislikeLoading(false);
+    }
+  };
+
+  return { handleLikeUnlikeReviewClick, isLikeDislikeLoading };
+};
