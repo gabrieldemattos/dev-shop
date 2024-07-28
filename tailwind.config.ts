7943c60884ef055cdfce1bc3036e7eac670b7968
Gabriel de Mattos <mattos_95@hotmail.com>
2024-07-28 16:37:33 -0300

    style: add permanent marker font to globals css and add wiggle animation to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,10 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        wiggle: {
+          "0%": { transform: "translateX(-500%)", opacity: "0" },
+          "100": { transform: "translateX(0%)", opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
