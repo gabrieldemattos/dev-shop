7d7a55186f2363ee6a63f22726019ce560dbc05c
Gabriel de Mattos <mattos_95@hotmail.com>
2024-07-29 17:48:29 -0300

    refactor: update search server action to include product slug, category, and category slug

--- app/search/_actions/search.ts
+++ app/search/_actions/search.ts
@@ -5,10 +5,36 @@ import { db } from "@/app/_lib/prisma";
 export const searchProducts = async (query: string) => {
   const products = await db.product.findMany({
     where: {
-      name: {
-        contains: query,
-        mode: "insensitive",
-      },
+      OR: [
+        {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          },
+        },
+        {
+          slug: {
+            contains: query,
+            mode: "insensitive",
+          },
+        },
+        {
+          category: {
+            name: {
+              contains: query,
+              mode: "insensitive",
+            },
+            OR: [
+              {
+                slug: {
+                  contains: query,
+                  mode: "insensitive",
+                },
+              },
+            ],
+          },
+        },
+      ],
     },
     include: {
       category: true,
