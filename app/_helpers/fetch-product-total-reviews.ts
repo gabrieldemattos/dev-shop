b214aac83ef935edb95feed365652926c75fa112
Gabriel de Mattos <mattos_95@hotmail.com>
2024-07-31 17:22:06 -0300

    refactor: create helper function to fetch total reviews for products

---
+++ app/_helpers/fetch-product-total-reviews.ts
@@ -0,0 +1,30 @@
+import { Prisma } from "@prisma/client";
+import { db } from "../_lib/prisma";
+
+export const fetchProductTotalReviews = async (
+  products: Prisma.ProductGetPayload<{
+    include: {
+      category: {
+        select: {
+          slug: true;
+        };
+      };
+    };
+  }>[],
+) => {
+  const productsWithTotalReviews = await Promise.all(
+    products.map(async (product) => {
+      const totalReviews = await db.review.count({
+        where: {
+          productId: product.id,
+        },
+      });
+      return {
+        ...product,
+        totalReviews,
+      };
+    }),
+  );
+
+  return productsWithTotalReviews;
+};
