76edf6c2c798f33282f60a19a1be909ae70c1c71
Gabriel de Mattos <mattos_95@hotmail.com>
2024-08-02 16:17:06 -0300

    feat: add isAdmin property to user authentication and NextAuth

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -17,6 +17,7 @@ export const authOptions: AuthOptions = {
       session.user = {
         ...session.user,
         id: user.id,
+        isAdmin: user.isAdmin,
       };
       return session;
     },
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,6 +4,12 @@ declare module "next-auth" {
   interface Session {
     user: {
       id?: string;
+      isAdmin?: boolean;
     } & DefaultSession["user"];
+  }
+
+  interface User {
+    id?: string;
+    isAdmin?: boolean;
   }
 }
