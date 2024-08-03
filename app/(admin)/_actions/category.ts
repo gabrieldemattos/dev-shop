4354b7a52cfc2e090c674bc634e03c8c3fb86bf3
Gabriel de Mattos <mattos_95@hotmail.com>
2024-08-03 18:26:10 -0300

    feat(admin): add CRUD operations for categories in admin panel

---
+++ app/(admin)/_actions/category.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { Prisma } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+export const createNewCategory = async (data: Prisma.CategoryCreateInput) => {
+  await db.category.create({ data });
+
+  revalidatePath("/dashboard/categories");
+};
+
+export const getCategory = async (id: string) => {
+  return await db.category.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const deleteCategory = async (id: string) => {
+  await db.category.delete({
+    where: {
+      id,
+    },
+  });
+
+  revalidatePath("/dashboard/categories");
+};
+
+export const updateCategory = async (data: Prisma.CategoryUpdateInput) => {
+  await db.category.update({
+    where: {
+      id: data.id as string,
+    },
+    data,
+  });
+
+  revalidatePath("/dashboard/categories");
+};
